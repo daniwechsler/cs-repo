53f2ee35b773499c63ff81e54fc8c6317e27182f
ortega <daniwechsler@bleumail.ch>
2013-09-26 14:56:58 +0200

    Integers Problem 2

--- integers/cs-problem-5.py
+++ integers/cs-problem-5.py
@@ -3,77 +3,15 @@
 # Author: Daniel Wechsler
 #
 
+import sys
 import numpy as np
 import matplotlib.pyplot as plt
 import math
+import findPrimeNumbers as prime
 
-#
-# The function finds all primes below 'n' using the  sieve of Eratosthenes
-# method. It returns a list containing all primes <= n.
-#
-# Idea:
-# 1)    Init list of integers [2...n]
-#
-# 2)    Select current element (At start point its the first
-#       element - later the next one to the right of the
-#       previously selected element. If there is no such
-#       element any more we are done and there are only primes
-#       in our list.
-#
-# 3)    Remove all multiples of the selected element from the list.
-#
-# 4)    Continue with step 2)
-#
-# Notes:
-# -     List holding none primes is actually not needed in this
-#       implementation.
-#
-def findPrimeNumbers (n):
-
-    
-    try:
-
-        if n < 2:
-            return []
-
-        # Initialize list that will hold all primes in [2,...n].
-        # At init time it holds all natural numbers in the interval.
-        primes          = range(2, n+1)
-          
-        # Initialize list holding none primes
-        nonePrimes      = []
-
-        currentIndex    = 0   
-
-        while True:
-            
-            number                  = primes[currentIndex]
-            
-            index = currentIndex + 1
-            while True:
-                   
-                length = len(primes)
-                if index == length: 
-                    break
-                
-                if primes[index] % number == 0: # The current number in the list is multiple of prime -> put it on none prime list
-                    nonePrimes.append(primes.pop(index))
-                else:
-                    index = index+1 # Current number is not a multiple. Keep it on prime list
-               
-                            
-            currentIndex = currentIndex + 1
-            if len(primes)-1 == currentIndex:
-                break
-
-        return primes            
-         
-    except TypeError: # Return empty list if given parameter n is not valid
-        return []
-    
-    
+        
 n =  10000
-primes = findPrimeNumbers(n)
+primes = prime.findPrimeNumbers(n)
 
 print ("Primes smaller or equal to: %i" % (n)) 
 print ("%s" % format(primes))
--- integers/cs-problem-6.py
+++ integers/cs-problem-6.py
@@ -4,5 +4,42 @@
 #
 
 
-k = pow (4, 2, 3)
-print (k)
+import findPrimeNumbers as prime
+import math
+
+# Finding Carmichael number using Eratosthenes sieve
+
+N                   = 20000;  # We search for Carmichael numbers lower than N
+primes              = prime.findPrimeNumbers(N)
+primeNumberIndex    = 0      # Used to keep track of which of the numbers below N are primes   
+carmichaelNumbers   = []
+for q in range(2, N) :
+
+    if q == primes[primeNumberIndex]: # q is a prime, thus no Carmichael number -> skip it
+        primeNumberIndex = primeNumberIndex + 1
+        if primeNumberIndex == len(primes):
+            break
+        continue
+
+    # Check for all coprimes of q 
+    isCarmichael = True
+    for p in primes:
+        if q % p != 0:
+            if pow(p, q-1, q) != 1:
+                isCarmichael = False
+                break
+                
+    if isCarmichael:
+      carmichaelNumbers.append(q)
+    
+   
+                
+
+print ("Carmichael numbers below N are:")
+print ("%s" % format(carmichaelNumbers))
+
+
+
+
+
+
