2fe70586822cbcafa4f71dc15e67bb5f00cc6f4e
ortega <daniwechsler@bleumail.ch>
2013-11-10 14:52:31 +0100

    update test.py

--- differential-equations/test.py
+++ differential-equations/test.py
@@ -1,19 +1,134 @@
+#
+# CS Problem 26
+# Author: Daniel Wechsler
+#
+
+#
+# A screen saver drawing trajectories following the Lorenz equation.
+# It's based on vtk (see http://www.vtk.org)
+#
+
 
 import vtk
 import random
 from scipy.integrate import odeint
 
 
-
+#
+# The camara shooting the sceene. Inherits
+# from vtkCamera and adds the update method
+# which moves the camera.
+#
 class Camera (vtk.vtkCamera) :
+
+    # Determines the focus point of the camera
+    # the object needs properties x, y, z
+    focusPointDet = None
+
+    pitch = 0.5
+
+    def __init__(self) :
+        self.SetFocalPoint(0, 0, 0)
+        self.SetPosition(60, 60, 60)
+
+    def update (self) :
+        self.updatePosition()
+        self.updateFocusPoint()
+
+    #
+    # Updates the position of the camera. Let it rotate
+    # around the focus point
+    #
+    def updatePosition (self) :       
+        self.Azimuth 	 (self.pitch)		
+
+    #
+    # Updates the focus point such that the camera points
+    # to the x,y,z valus of the focusPointDet object.
+    # 
+    def updateFocusPoint (self) :
+        if self.focusPointDet != None :
+             self.SetFocalPoint(self.focusPointDet.x, self.focusPointDet.y, self.focusPointDet.z)   
+        
+
+
+
+#
+# The clas is responsible for drawing a trajectroy
+# following the Lorenz equation an thus produce an
+# image of the Lorenz attractor. 
+#
+class LorenzTrajectory (vtk.vtkAssembly) :
+
+    STEP_SIZE = 0.015       # Step size used to compuet next position
     
-    def updatePosition () :
-        return 1
+    x = 1.0
+
+    y = 1.0
+    
+    z = 1.0
+
+    color = [0, 1, 0]
+
+    #
+    # Init a random starting point and choose
+    # a random color.
+    #
+    def __init__(self) :
+        self.x          = random.uniform(-4, 4)
+        self.y          = random.uniform(-4, 4)
+        self.z          = random.uniform(-4, 4)
+        self.color[0]   = random.random()
+        self.color[1]   = random.random()
+        self.color[2]   = random.random()
+        
+
+    def update (self) :
+
+        # Draw a line from current to next position.
+        source      = vtk.vtkLineSource()
+        source.SetPoint1(self.x,self.y,self.z)
+        self.computeNextPosition ()
+        source.SetPoint2(self.x,self.y,self.z)
+        mapper = vtk.vtkPolyDataMapper()
+        mapper.SetInput(source.GetOutput())
+        actor = vtk.vtkActor()
+        actor.SetMapper(mapper)
+        # Visual effects
+        actor.GetProperty().SetColor(self.color)
+        actor.GetProperty().SetOpacity (0.6)
+        self.AddPart(actor)
+        
+
+    #
+    # For the corrent x,y,z position the method computes
+    # the next position according to the Lorenz equation
+    # using scipy odeint.
+    #
+    def computeNextPosition (self) :
+
+        v = [self.x, self.y, self.z]
+        def lorenz (y, x) :
+            r       = 28
+            b       = 8/3
+            sigma   = 10
+            dx      = sigma*(y[1]-y[0])
+            dy      = r*y[0]-y[1]-y[0]*y[2]
+            dz      = y[0]*y[1] - b * y[2]
+            return [dx, dy, dz]
+        
+        r = odeint(lorenz, v, [0.0, self.STEP_SIZE])
+        self.x = r[1][0]
+        self.y = r[1][1]
+        self.z = r[1][2]
+        
 
 #
 # Represents the Lorenz attractor screen saver.
 #
 class ScreenSaver ():
+
+    MAX_ITERATIONS  = 1500 # Max number of iterations until reset of scene.
 
     renderer        = None
 
@@ -23,6 +138,10 @@ class ScreenSaver ():
 
     camera          = None
 
+    trajectory      = None
+
+    iteration       = 0
+
     #
     # Initialize the screen save.
     #
@@ -30,165 +149,72 @@ class ScreenSaver ():
         # Initialize the window and renderer
         self.renderer           = vtk.vtkRenderer()
         self.rendererWindow     = vtk.vtkRenderWindow()
-        self.rendererWindow.AddRenderer(ren)
+        
+        self.rendererWindow.AddRenderer(self.renderer)
         self.rendererWindow.SetSize (1200, 800)
 
-        renderInteract = vtk.vtkRenderWindowInteractor()
-        self.rendererWindow.SetRenderWindow(renderInteract)
-
-        # Initialize camera
+        self.renderInteract = vtk.vtkRenderWindowInteractor()
+        self.renderInteract.SetRenderWindow(self.rendererWindow)     
+        
+        # Cerate the camara observing the scene
         self.camera = Camera()
+        self.rendererWindow.Size;
 
-    def run (self) :
-        return 0
+        
 
-    
-
-
-
-class vtkTimerCallback():
-
-    x = 1.0
-    y = 1.0
-    z = 1.0
-
-    assembly = None
-    
-    def __init__(self):
-        self.timer_count = 0
- 
-    def execute(self,obj,event):
-        self.appendLine()
+    #
+    # Puts trajectories and the camara into the scene
+    #
+    def setUpScene (self) :
+        # Init the trajectory
+        self.trajectory = LorenzTrajectory()
+        self.renderer.AddActor(self.trajectory)
        
+        # Initialize camera position
+        self.camera.focusPointDet = self.trajectory
+        self.renderer.SetActiveCamera(self.camera)
+         
+    #
+    # Removes all actors from the scene. Used before reinitialization.
+    # 
+    def tearDownScene (self) :
+        self.renderer.RemoveActor(self.trajectory);
 
-    def updateCamera (self) :
-        self.camera.SetFocalPoint(self.x,self.y,self.z)
 
-    def appendLine (self) :
-
+    def resetScene (self) :
+        self.tearDownScene()
+        self.setUpScene()
+        iteration = 0
         
+
+    #
+    # The main loop of the screen saver
+    #
+    def run (self) :
+        self.setUpScene()
+        self.renderInteract.Initialize()
+        self.rendererWindow.Render()
+   
+        # Init a timer event that periodically calls the update method
+        self.renderInteract.AddObserver('TimerEvent', self.update)
+        self.renderInteract.CreateRepeatingTimer(10);
+        self.renderInteract.Start()
+
+
+    #
+    # Periodically called method the updates the scene
+    # 
+    def update (self, obj,event) :
+        self.trajectory.update()
         
-        source = vtk.vtkLineSource()
-        source.SetPoint1(self.x,self.y,self.z)
-        self.computeNext ()
-        source.SetPoint2(self.x,self.y,self.z)
-        mapper = vtk.vtkPolyDataMapper()
-        mapper.SetInput(source.GetOutput())
-        actor = vtk.vtkActor()
-        actor.SetMapper(mapper)
-        actor.GetProperty().SetColor(0,0,1)
-        
-        self.assembly.AddPart(actor)
+        self.camera.update()
+        self.renderInteract.GetRenderWindow().Render()
+        self.iteration += 1
+        # Reset the scene after some iterations (rendering becomse too slow otherwise) 
+        if self.iteration % self.MAX_ITERATIONS == 0 : 
+            self.resetScene()
+    
 
+screenSaver = ScreenSaver()
+screenSaver.run()
 
-        #self.rendered.AddActor(actor)
-        iren.GetRenderWindow().Render()
-        self.timer_count += 1
-
-        if self.timer_count % 100 == 0 :
-            #self.rendered.RemoveActor(self.assembly)
-            self.fadeOut()
-                   
-        self.updateCamera()
-
-
-    def fadeOut (self) :
-        return 1
-        #self.assembly.GetProperty().SetColor(1,0,1)
-
-        
-    def computeNext (self) :
-        #self.x += random.uniform(-0.1, 0.1)
-        #self.y += random.uniform(-0.1, 0.1)
-        #self.z += random.uniform(-0.1, 0.1)
-        v = [self.x, self.y, self.z]
-        
-        def lorenz (y, x) :
-           
-            r       = 28
-            b       = 8/3
-            sigma   = 10
-            dx      = sigma*(y[1]-y[0])
-            dy      = r*y[0]-y[1]-y[0]*y[2]
-            dz      = y[0]*y[1] - b * y[2]
-            
-            return [dx, dy, dz]
-
-        r = odeint(lorenz, v, [0.0, 0.01])
-        self.x = r[1][0]
-        self.y = r[1][1]
-        self.z = r[1][2]
-
-
-# create a rendering window and renderer
-ren = vtk.vtkRenderer()
-renWin = vtk.vtkRenderWindow()
-renWin.AddRenderer(ren)
-renWin.	SetSize (1200, 800)
- 
-# create a renderwindowinteractor
-iren = vtk.vtkRenderWindowInteractor()
-iren.SetRenderWindow(renWin)
- 
- 
-# create source
-source = vtk.vtkLineSource()
-source.SetPoint1(0,0,0)
-source.SetPoint2(-5.8,-5,-5)
- 
-# mapper
-mapper = vtk.vtkPolyDataMapper()
-mapper.SetInput(source.GetOutput())
- 
-# actor
-actor = vtk.vtkActor()
-actor.SetMapper(mapper)
-
-
-
- 
-# color actor
-actor.GetProperty().SetColor(1,0,1)
- 
-# assign actor to the renderer
-#ren.AddActor(actor)
-
-sphereSource = vtk.vtkSphereSource()
-sphereSource.SetCenter(0.0, 0.0, 0.0)
-sphereSource.SetRadius(0.001)
-
-sphereMapper = vtk.vtkPolyDataMapper()
-sphereMapper.SetInputConnection(sphereSource.GetOutputPort())
-sphereActor = vtk.vtkActor()
-sphereActor.SetMapper(sphereMapper)
-
-#ren.AddActor(sphereActor)
-
-
-transform = vtk.vtkTransform()
-transform.Translate(1.0, 0.0, 0.0)
- 
-camera = vtk.vtkCamera ()
-camera.SetPosition(0, 0,0)
-camera.SetFocalPoint(0, 0, 0)
-
-#ren = vtk.vtkRenderer();
-ren.SetActiveCamera(camera)
-
-iren.Initialize()
-renWin.Render()
-
-cb = vtkTimerCallback()
-cb.actor = actor
-cb.rendered = ren
-cb.camera = camera
-assembly = vtk.vtkAssembly()
-cb.assembly = assembly
-ren.AddActor(assembly)
-
-iren.AddObserver('TimerEvent', cb.execute)
-timerId = iren.CreateRepeatingTimer(10);
-
-# enable user interface interactor
-
-iren.Start()
